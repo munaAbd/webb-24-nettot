96ab57a2ced41ecc3ba333c62f1a2561b4c86098
Muna <muna.abdi@gritacademy.se>
2024-10-01 10:30:18 +0200

    added all

---
+++ docs/index.32a5dfed.js
@@ -0,0 +1,2 @@
+console.log("mhgcghc"),document.querySelector("h1").style.color="hotpink";
+//# sourceMappingURL=index.32a5dfed.js.map
---
+++ src/modules/main.js
@@ -0,0 +1,4 @@
+console.log (
+    'mhgcghc')
+    const h1 = document.querySelector('h1');
+    h1.style.color = 'hotpink';
